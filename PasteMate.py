eb9a9b17f59ed2a7e5043ae100bfe25819a5077a
awilliams17 <awilliams17411@gmail.com>
2018-08-25 05:42:20 -0500

    Navbar done

--- PasteMate.py
+++ PasteMate.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from flask_session import Session
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+SESSION_TYPE = 'redis'
+app.config.from_object(__name__)
+Session(app)
+app.debug = True
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template("base.html")
 
 
 if __name__ == '__main__':
+    session = Session()
+    session.init_app(app)
     app.run()
