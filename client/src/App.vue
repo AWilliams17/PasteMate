06b4991592658fa7085b8cb86ab93238c78d1f01
awilliams17 <awilliams17411@gmail.com>
2018-09-01 20:33:04 -0500

    Set up webpack, bootstrap vue, bootswatch, etc

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -11,13 +10,8 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import './styles/variables.scss';
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
+  @import './styles/bootswatch.scss';
 </style>
