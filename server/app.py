06cb918e9bbfd649e43525abc02a227057f47cd9
awilliams17 <awilliams17411@gmail.com>
2018-09-27 11:41:21 -0500

    working on registration

--- server/app.py
+++ server/app.py
@@ -4,13 +4,16 @@ from models import db, Account, Paste
 from flask_session import Session
 from os import path, scandir
 
+
 app = Flask(__name__, template_folder="../client/")
 
 # SESSION_TYPE = 'redis'
 app.config.from_object(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     "sqlite:///" + path.dirname(path.realpath(__file__)) + "\PM.db".replace('"\"', '\\')  # ugh
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECURITY_PASSWORD_SALT'] = "12345"  # ToDo: Bad.
+app.config['SECRET_KEY'] = "12345"  # ToDo: Bad.
 
 with app.app_context():
     db.init_app(app)
@@ -24,6 +27,15 @@ CORS(app)  # ToDo: Secure this. This allows CORS requests on all routes from any
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    user = Account(**data)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.to_dict()), 201
 
 
 @app.route('/', defaults={'path': ''})
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -9,11 +10,38 @@ class Account(db.Model):
     username = db.Column(db.String(24), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    pastes = db.relationship('Paste', backref='account', lazy=True)
+    pastes = db.relationship('Paste', backref='account', order_by='paste.id', cascade="all, delete-orphan", lazy=True)
     private = db.Column(db.Boolean, nullable=True)
+    confirmed = db.Column(db.Boolean, nullable=False, default=False)
+
+    @classmethod
+    def authenticate(cls, **kwargs):
+        username = kwargs.get('username')
+        email = kwargs.get('email')
+        password = kwargs.get('password')
+
+        if not username or not email or not password:
+            return None
+
+        user = cls.query.filter_by(username=username).first()
+        if not user or not check_password_hash(user.password, password):
+            return None
+
+        return user
+
+    def to_dict(self):
+        return dict(id=self.id, username=self.username)
 
     def __repr__(self):
         return '<Account %r>' % self.username
+
+    def __init__(self, username, email, password, private):
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password, method='sha256')
+        self.private = private
+        self.pastes = None
+        self.confirmed = False
 
 
 class Paste(db.Model):
@@ -27,6 +55,20 @@ class Paste(db.Model):
     edit_date = db.Column(db.DateTime, nullable=True)
     open_edit = db.Column(db.Boolean, nullable=False)
     expiration_date = db.Column(db.DateTime, nullable=True)
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            owner=self.owner_id,
+            title=self.title,
+            language=self.language,
+            password=self.password,
+            content=self.content,
+            submission_date=self.submission_date,
+            edit_date=self.edit_date,
+            open_edit=self.open_edit,
+            expiration_date=self.expiration_date
+        )
 
     def __repr__(self):
         return '<Paste %r>' % self.title
